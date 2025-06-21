02fd23b4e7731ffd83adefd8504a9b30675140ee
Niklas <Max_Niklas@outlook.de>
2025-06-21 17:01:16 +0200

    Implement TreeBuilder for both, isA and partOf trees

---
+++ src/main/java/explorer/model/treeBuilder/TreeBuilder.java
@@ -0,0 +1,178 @@
+package explorer.model.treeBuilder;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
+import explorer.model.AnatomyNode;
+import org.objenesis.strategy.StdInstantiatorStrategy;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * This class was used only once to serialize the AnatomyNode tree to a kryo file for high performance tree Loading
+ */
+class TreeBuilder {
+
+    /**
+     * Main method to serialize both the is-a and part-of trees.
+     * Invokes the methods that build and save the respective trees to Kryo files.
+     * @param args command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        serializeIsATree();
+        serializePartOfTree();
+    }
+
+    /**
+     * Builds and serializes the "is-a" anatomical tree.
+     * Loads relationships and element parts, constructs the tree rooted at "FMA62955",
+     * prints the tree in Newick format, and saves it using Kryo serialization.
+     */
+    private static void serializeIsATree() {
+        LinkedList<Relation> relations = loadRelationsFile("src/main/resources/tree/isA/isa_inclusion_relation_list.txt");
+        HashMap<String, LinkedList<String>> conceptIDToFileID = loadElementFile("src/main/resources/tree/isA/isa_element_parts.txt");
+        AnatomyNode tree = createTree(relations, conceptIDToFileID, "FMA62955"); // root concept == "FMA62955" (anatomical entity)
+        System.out.println(tree.toNewick()); // control tree
+
+        freezeTree(tree, "src/main/resources/serializedTress/isA_tree.kryo");
+    }
+
+    /**
+     * Builds and serializes the "part-of" anatomical tree.
+     * Loads relationships and element parts, constructs the tree rooted at "FMA20394",
+     * prints the tree in Newick format, and saves it using Kryo serialization.
+     */
+    private static void serializePartOfTree() {
+        LinkedList<Relation> relations = loadRelationsFile("src/main/resources/tree/partOf/partof_inclusion_relation_list.txt");
+        HashMap<String, LinkedList<String>> conceptIDToFileID = loadElementFile("src/main/resources/tree/partOf/partof_element_parts.txt");
+        AnatomyNode tree = createTree(relations, conceptIDToFileID, "FMA20394"); // root concept == "FMA20394" (human body)
+        System.out.println(tree.toNewick()); // control tree
+
+        freezeTree(tree, "src/main/resources/serializedTress/partOf_tree.kryo");
+    }
+
+    private record Relation(String parentID, String parentName, String childID, String childName){}
+
+    /**
+     * Loads a tab-separated values file containing hierarchical anatomical relations.
+     * Each line contain: parentID, parentName, childID, childName.
+     * Skips the header and returns a list of Relation records.
+     *
+     * @param filePath path to the relation file
+     * @return list of Relation objects
+     *
+     * SOURCE: assignment02
+     */
+    private static LinkedList<Relation> loadRelationsFile(String filePath) {
+        LinkedList<Relation> relations = new LinkedList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            reader.readLine(); // skip header
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\t");
+                String parentID = parts[0].trim();
+                String parentName = parts[1].trim();
+                String childID = parts[2].trim();
+                String childName = parts[3].trim();
+                relations.add(new Relation(parentID, parentName, childID, childName));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return relations;
+    }
+
+    /**
+     * Loads a mapping from ConceptID to a list of associated FileIDs from a tab-separated file.
+     * Each line must contain at least three fields, the third of which is stored.
+     *
+     * @param filePath path to the file containing ConceptID to FileID mappings
+     * @return HashMap of ConceptID to list of FileIDs or null if an error occurs
+     *
+     * SOURCE: assignment02
+     */
+    private static HashMap<String, LinkedList<String>> loadElementFile(String filePath) {
+        HashMap<String, LinkedList<String>> IDtoFilelist = new HashMap<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            if ((reader.readLine()==null)) {
+                System.err.println("TreeLoader: load(): elementsFile empty.");
+                return null;
+            }
+            String line;
+            while ((line=reader.readLine()) != null) {
+                String[] lineArr = line.split("\t");
+                String conceptID = lineArr[0].trim();
+                LinkedList<String> fileList = IDtoFilelist.containsKey(conceptID) ? IDtoFilelist.get(conceptID) : new LinkedList<String>();
+                fileList.add(lineArr[2].trim());
+                IDtoFilelist.put(conceptID, fileList);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("loadElement: elementsFile not found.");
+            return null;
+        } catch (IOException e) {
+            System.err.println("loadElement: load(): Something happened with elementsFile");
+            return null;
+        }
+        return IDtoFilelist;
+    }
+
+    /**
+     * Constructs a tree of AnatomyNode objects based on a list of relations and ConceptID to FileID mappings.
+     * Each node is connected to its parent and children based on the relation list.
+     *
+     * @param relations list of parent-child relations
+     * @param conceptIDToFileID map of ConceptID to FileIDs
+     * @param rootConceptID the ID of the root node
+     * @return root AnatomyNode of the constructed tree
+     *
+     * SOURCE: assignment01
+     */
+    private static AnatomyNode createTree(LinkedList<Relation> relations, HashMap<String, LinkedList<String>> conceptIDToFileID, String rootConceptID) {
+        HashMap<String, AnatomyNode> idToNode = new HashMap<>();
+
+        for (Relation relation : relations) {
+            String parentID = relation.parentID();
+            String parentName = relation.parentName();
+            String childID = relation.childID();
+            String childName = relation.childName();
+
+            AnatomyNode parentNode = idToNode.getOrDefault(parentID, new AnatomyNode(parentID, parentName, new LinkedList<>(), conceptIDToFileID.get(parentID)));
+            AnatomyNode childNode = idToNode.getOrDefault(childID, new AnatomyNode(childID, childName, new LinkedList<>(), conceptIDToFileID.get(childID)));
+            parentNode.addChild(childNode);
+            idToNode.putIfAbsent(relation.parentID(), parentNode);
+            idToNode.putIfAbsent(relation.childID(), childNode);
+        }
+
+        return idToNode.get(rootConceptID);
+    }
+
+    /**
+     * Serializes the given AnatomyNode tree to a file using Kryo for efficient deserialization later.
+     *
+     * @param tree the AnatomyNode tree to serialize
+     * @param saveToPath the path where the Kryo file will be saved
+     */
+    public static void freezeTree(AnatomyNode tree, String saveToPath) {
+        // Freeze the tree
+        Kryo kryo = new Kryo();
+        kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());  // Optional, wenn du final-Felder hast
+
+        // (Optional) Klassen registrieren, für bessere Performance
+        kryo.register(AnatomyNode.class);
+        kryo.register(LinkedList.class);
+        kryo.register(String.class);
+
+        // Speicherort
+        try (Output output = new Output(new FileOutputStream(saveToPath))) {
+            kryo.writeObject(output, tree);
+            System.out.println("Kryo file successfully generated!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
